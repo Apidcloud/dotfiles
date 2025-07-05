7e2b870481e20b74ea18560c4fbe37c4908f29c6
Luis <luis@artivive.com>
2025-07-05 20:08:54 +0200

    feat: auto format on save

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,64 +1,85 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		opts = {},
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		opts = {
-			-- auto_install not really working?
-			--auto_install = true,
-			ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "clangd" },
-		},
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "mason-org/mason.nvim",
+    opts = {},
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      -- auto_install not really working?
+      --auto_install = true,
+      ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "clangd" },
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.rust_analyzer.setup({
-				-- Server-specific settings. See `:help lspconfig-setup`
-				capabilities = capabilities,
-				settings = {
-					["rust-analyzer"] = {},
-				},
-			})
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
+      local lspconfig = require("lspconfig")
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.rust_analyzer.setup({
+        -- Server-specific settings. See `:help lspconfig-setup`
+        capabilities = capabilities,
+        settings = {
+          ["rust-analyzer"] = {},
+        },
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+      })
 
-			vim.diagnostic.config({
-				virtual_lines = true,
-				--virtual_text = true,      -- Show inline error/warning text
-				underline = true,
-				update_in_insert = false,
-				severity_sort = true,
-				float = {
-					border = "rounded",
-					source = "true",
-				},
-			})
+      vim.diagnostic.config({
+        virtual_lines = true,
+        --virtual_text = true,      -- Show inline error/warning text
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          border = "rounded",
+          source = "true",
+        },
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
-		end,
-	},
+      -- auto format on save
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("LspFormatOnSave", {}),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if
+              client
+              and not client.supports_method("textDocument/willSaveWaitUntil")
+              and client.supports_method("textDocument/formatting")
+          then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = vim.api.nvim_create_augroup("LspFormatOnSave", { clear = false }),
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+              end,
+            })
+          end
+        end,
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+    end,
+  },
 }
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,12 +1,27 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
+    local eslint_d = require("none-ls.diagnostics.eslint_d")
+    local sources = {
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.biome.with({
+        condition = function(utils)
+          return utils.root_has_file({ "biome.json" })
+        end,
+      }),
+      eslint_d.with({
+        condition = function(utils)
+          return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json", ".eslintrc", "eslint.config.js" })
+        end,
+      }),
+    }
+
     null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.biome,
-      },
+      sources = sources,
     })
 
     vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
