d3d6bf4b2c2ac2baf90e6011898c72a1a0ca06fc
Luis <luis@artivive.com>
2026-04-24 19:06:08 +0200

    fix(tmux-cht): sue less -R to correctly interpret and render ANSI color escape sequences

--- .local/bin/tmux-cht.sh
+++ .local/bin/tmux-cht.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
-selected=`cat ~/.local/bin/.cht-languages ~/.local/bin/.cht-commands | fzf`
-if [[ -z $selected ]]; then
+
+selected=$(cat ~/.local/bin/.cht-languages ~/.local/bin/.cht-commands | fzf)
+if [[ -z "$selected" ]]; then
     exit 0
 fi
 
-read -p "Query: " query
+read -r -p "Query: " query
 
 if grep -qs "$selected" ~/.local/bin/.cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    encoded_query=$(printf '%s' "$query" | tr ' ' '+')
+    url="cht.sh/$selected/$encoded_query/"
+    printf -v escaped_url '%q' "$url"
+    tmux neww "bash -lc 'echo curl $escaped_url; curl $escaped_url; while :; do sleep 1; done'"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    url="cht.sh/$selected~$query"
+    printf -v escaped_url '%q' "$url"
+    tmux neww "bash -lc 'curl -s $escaped_url | less -R'"
 fi
-
